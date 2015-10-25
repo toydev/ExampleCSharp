3309c03d8b9c973db085bf3941b2050dd27a9041
toydev <toy@ownway.info>
2015-10-25 10:01:55 +0900

    メッセージボックスを表示するサンプルを追加した。

---
+++ WPF/MessageBox/SimpleMessageBox/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowMessageBoxButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("メッセージボックスに表示するメッセージをここに入力する。", "メッセージボックスのタイトル");
+        }
+    }
+}
