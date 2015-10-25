602bd0be061736ec7c2cea6c2e4cbaf65162f4c0
toydev <toy@ownway.info>
2015-10-25 10:41:49 +0900

    カスタムプログレスダイアログのサンプルを追加した。

---
+++ WPF/MessageBox/CustomProgressDialog/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+
+namespace CustomProgressDialog
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowProgressDialogButton_Click(object sender, RoutedEventArgs e)
+        {
+            ProgressDialog dialog = new ProgressDialog(
+                this,
+                "プログレスダイアログのタイトル",
+                "プログレスダイアログに表示するメッセージをここに入力する。",
+                new System.Action<ProgressDialog>((x) =>
+                {
+                    // 処理内容をここに入力する
+                    for (int i = 0; i < 100; ++i)
+                    {
+                        Thread.Sleep(100);
+                        x.NotifyProgress(0, 100, i);
+                    }
+                }));
+            dialog.ShowDialog();
+        }
+    }
+}
---
+++ WPF/MessageBox/CustomProgressDialog/ProgressDialog.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+
+namespace CustomProgressDialog
+{
+
+    /// <summary>
+    /// ProgressDialog.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ProgressDialog : Window
+    {
+
+        System.Action<ProgressDialog> m_action;
+
+        public ProgressDialog(Window parent, string title, string message, System.Action<ProgressDialog> action)
+        {
+            InitializeComponent();
+
+            this.Owner = parent;
+            this.Title = title;
+            messageLabel.Text = message;
+            m_action = action;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Task task = Task.Factory.StartNew(() =>
+            {
+                m_action.Invoke(this);
+                Dispatcher.BeginInvoke(new System.Action(() =>
+                {
+                    DialogResult = true;
+                }));
+            });
+        }
+
+        public void NotifyProgress(int min, int max, int value)
+        {
+            Dispatcher.BeginInvoke(new System.Action(() =>
+            {
+                progressBar.Minimum = min;
+                progressBar.Maximum = max;
+                progressBar.Value = value;
+            }));
+        }
+    }
+
+}
