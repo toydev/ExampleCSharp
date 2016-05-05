26f0c7ec1f2ded80ce90e3ae30a440615283f040
toydev <toy@ownway.info>
2016-05-05 12:59:23 +0900

    OxyPlot のサンプルを追加した。

---
+++ WPF/OxyPlot/SimpleGraph1/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace SimpleGraph1
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindowViewModel ViewModel = new MainWindowViewModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = ViewModel;
+        }
+    }
+}
---
+++ WPF/OxyPlot/SimpleGraph1/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+using OxyPlot;
+
+namespace SimpleGraph1
+{
+    public class MainWindowViewModel
+    {
+        public IList<DataPoint> GraphData { get; set; } = new List<DataPoint>();
+
+        public MainWindowViewModel()
+        {
+            GraphData.Add(new DataPoint(1.0, 1.0));
+            GraphData.Add(new DataPoint(2.0, 3.0));
+            GraphData.Add(new DataPoint(3.0, 2.0));
+        }
+    }
+}
