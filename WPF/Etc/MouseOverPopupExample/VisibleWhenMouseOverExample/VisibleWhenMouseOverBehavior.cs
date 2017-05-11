1848ccf1b5ad71ac398a7ed27886931f513a9c2f
toy <toy@ownway.info>
2017-05-11 09:53:00 +0900

    マウスオーバーした場合にだけ表示するボタン等を作るための Behavior サンプルを追加した。

---
+++ WPF/Etc/MouseOverPopupExample/VisibleWhenMouseOverExample/VisibleWhenMouseOverBehavior.cs
@@ -0,0 +1,72 @@
+﻿using System.Windows;
+using System.Windows.Interactivity;
+
+namespace VisibleWhenMouseOverExample
+{
+    public class VisibleWhenMouseOverBehavior : Behavior<FrameworkElement>
+    {
+        protected override void OnAttached()
+        {
+            OnVisibilityChanged();
+        }
+
+        protected void OnVisibilityChanged()
+        {
+            AssociatedObject.Visibility =
+                TargetMouseOver || SelfMouseOver
+                ? Visibility.Visible : Visibility.Hidden;
+        }
+
+
+        #region TargetMouseOverProperty
+        public static readonly DependencyProperty TargetMouseOverProperty =
+            DependencyProperty.Register(
+                "TargetMouseOver", typeof(bool), typeof(VisibleWhenMouseOverBehavior),
+                new FrameworkPropertyMetadata(
+                    false
+                    , new PropertyChangedCallback(OnVisibilityChanged)
+                )
+            );
+
+        public bool TargetMouseOver
+        {
+            get
+            {
+                return (bool)base.GetValue(TargetMouseOverProperty);
+            }
+            set
+            {
+                base.SetValue(TargetMouseOverProperty, value);
+            }
+        }
+        #endregion
+
+        #region SelfMouseOverProperty
+        public static readonly DependencyProperty SelfMouseOverProperty =
+            DependencyProperty.Register(
+                "SelfMouseOver", typeof(bool), typeof(VisibleWhenMouseOverBehavior),
+                new FrameworkPropertyMetadata(
+                    false
+                    , new PropertyChangedCallback(OnVisibilityChanged)
+                )
+            );
+
+        public bool SelfMouseOver
+        {
+            get
+            {
+                return (bool)base.GetValue(SelfMouseOverProperty);
+            }
+            set
+            {
+                base.SetValue(SelfMouseOverProperty, value);
+            }
+        }
+        #endregion
+
+        private static void OnVisibilityChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            (d as VisibleWhenMouseOverBehavior)?.OnVisibilityChanged();
+        }
+    }
+}
